0dd373a5f0486a1f1d40da7acd6b195fe32724f3
Tomas Miliauskas <tomas@wix.com>
2017-05-16 11:05:53 +0300

    Add Part 4, Step 2

---
+++ part-4-testing-react-apps/step-2/src/components/App/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import logo from './logo.png';
+import HiddenText from '../HiddenText';
+import Alphabet from '../Alphabet';
+import Hangman from '../Hangman';
+import {decrementLives, guessLetter, fetchData} from '../../actions';
+
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchData());
+  }
+
+  checkLetter(letter) {
+    const {text, lives, dispatch} = this.props;
+
+    if (lives === 0) {
+      return;
+    }
+
+    if (text.toLowerCase().indexOf(letter) === -1) {
+      dispatch(decrementLives());
+    }
+
+    dispatch(guessLetter(letter));
+  }
+
+  render() {
+    const {question, text, lives, guesses} = this.props;
+    
+    return (
+      <div>
+        <img src={logo} alt="Hangman Game"/>
+        <HiddenText question={question} text={text} guesses={guesses}/>
+        <Hangman lives={lives}/>
+        <Alphabet onLetterClick={this.checkLetter.bind(this)} guesses={guesses}/>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({question, text, lives, guesses}) {
+  return {question, text, lives, guesses};
+}
+
+export default connect(mapStateToProps)(App);
